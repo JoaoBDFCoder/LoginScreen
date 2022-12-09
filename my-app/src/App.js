f5b6b815e4fbfa0978b8b32c7ae4fb50c01db9d1
joaobdf <joaobdf@hotmail.com>
2022-12-09 09:33:53 -0300

    Iniciando Projeto Tela de login WoW

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; 
+import './App.css';
+
+import LoginScreen from './components/LoginScreen';
+import CameIn from './components/CameIn';
+
+function App () {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<LoginScreen />} />
+        <Route exact path="Sobre" element={<CameIn />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
